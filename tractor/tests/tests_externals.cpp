2e977e20325031b16eb95ea05ffc6dafc1551aa6
Erlend Elias Isachsen <erlend@isachsen.org>
2023-07-20 17:28:22 +0200

    Added googletest and implemented simple tests. Added GLM which will be used later. Introduced tests to check that externals works as intended.

---
+++ tractor/tests/tests_externals.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+
+#include <glm/glm.hpp>
+
+TEST(externals, glm_vec3_addition)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	glm::vec3 c = a + b;
+	EXPECT_EQ(c.x, 2.0f);
+	EXPECT_EQ(c.y, 4.0f);
+	EXPECT_EQ(c.z, 6.0f);
+}
+
+TEST(externals, glm_vec3_subtraction)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	glm::vec3 c = a - b;
+	EXPECT_EQ(c.x, 0.0f);
+	EXPECT_EQ(c.y, 0.0f);
+	EXPECT_EQ(c.z, 0.0f);
+}
+
+TEST(externals, glm_vec3_multiplication)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	glm::vec3 c = a * b;
+	EXPECT_EQ(c.x, 1.0f);
+	EXPECT_EQ(c.y, 4.0f);
+	EXPECT_EQ(c.z, 9.0f);
+}
+
+TEST(externals, glm_vec3_division)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	glm::vec3 c = a / b;
+	EXPECT_EQ(c.x, 1.0f);
+	EXPECT_EQ(c.y, 1.0f);
+	EXPECT_EQ(c.z, 1.0f);
+}
+
+TEST(externals, glm_vec3_dot)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	float c = glm::dot(a, b);
+	EXPECT_EQ(c, 14.0f);
+}
+
+TEST(externals, glm_vec3_cross)
+{
+	glm::vec3 a(1.0f, 2.0f, 3.0f);
+	glm::vec3 b(1.0f, 2.0f, 3.0f);
+	glm::vec3 c = glm::cross(a, b);
+	EXPECT_EQ(c.x, 0.0f);
+	EXPECT_EQ(c.y, 0.0f);
+	EXPECT_EQ(c.z, 0.0f);
+}
---
+++ tractor/tests/tests_tractor.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <tractor.hpp>
+
+
+TEST(tractor, test_always_pass)
+{
+	EXPECT_TRUE(true);
+}
